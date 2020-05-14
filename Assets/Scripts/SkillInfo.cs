dc305a1436071902d04faca37217eff8c8008b21
Babdodook <48229283+Babdodook@users.noreply.github.com>
2020-05-14 15:46:57 +0900

    Initial commit

---
+++ Assets/Scripts/SkillInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SkillType
+{
+    Combat=0,
+    Magic,
+    Passive,
+
+    Max
+}
+
+public class SkillInfo : MonoBehaviour
+{
+    SkillType type;
+
+    string name;
+    
+    
+}
