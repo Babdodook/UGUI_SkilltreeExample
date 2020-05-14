721a9734e05807ed89844795c74d3e2cba80493d
Babdodook <48229283+Babdodook@users.noreply.github.com>
2020-05-14 18:02:01 +0900

    스킬정보 정의

--- Assets/Scripts/SkillInfo.cs
+++ Assets/Scripts/SkillInfo.cs
@@ -4,18 +4,57 @@ using UnityEngine;
 
 public enum SkillType
 {
-    Combat=0,
-    Magic,
-    Passive,
+    Battlerage = 0,     // 격투
+    Sorcery,            // 마법
+    Archery,            // 야성
+    Shadowplay,         // 사명
+    Witchcraft,         // 환술
 
     Max
 }
 
 public class SkillInfo : MonoBehaviour
 {
-    SkillType type;
+    [Header("스킬 타입")]
+    public SkillType m_type;
 
-    string name;
+    [Header("스킬 정보")]
+    [Header("이름")]        public string m_name;
+    [Header("소모 활력")]   public int m_mana;
+    [Header("유효거리")]    public float m_range;
+    [Header("시전시간")]    public float m_castTime;
+    [Header("요구 레벨")]   public int m_requiredLevel;
+    [Header("스킬 설명")]   public string m_descryption;
+    [Header("스킬 개방")]   public bool m_isUnLocked;
     
     
+    public SkillInfo()
+    {
+        m_type = SkillType.Max;
+        m_name = "";
+        m_range = 0;
+        m_castTime = 0;
+        m_requiredLevel = 0;
+        m_isUnLocked = false;
+    }
+
+    public SkillInfo(   SkillType _type,
+                        string _name,
+                        int _mana,
+                        float _range,
+                        float _castTime,
+                        int _requiredLevel,
+                        string _descryption,
+                        bool _isUnlocked    )
+    {
+        m_type = _type;
+        m_name = _name;
+        m_mana = _mana;
+        m_range = _range;
+        m_castTime = _castTime;
+        m_requiredLevel = _requiredLevel;
+        m_descryption = _descryption;
+        m_isUnLocked = _isUnlocked;
+    }
 }
+
--- Assets/Scripts/SkillTreeManager.cs
+++ Assets/Scripts/SkillTreeManager.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class SkillTreeManager : MonoBehaviour
 {
+    List<SkillInfo> Battlerage;
+    List<SkillInfo> Sorcery;
+    List<SkillInfo> Archery;
+    List<SkillInfo> Shadowplay;
+    List<SkillInfo> Witchcraft;
+
     void Awake()
     {
 
@@ -12,5 +18,21 @@ public class SkillTreeManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // 스킬 리스트 초기화
+    void InitializeSkillList()
+    {
+        Battlerage = new List<SkillInfo>();
+        Sorcery = new List<SkillInfo>();
+        Archery = new List<SkillInfo>();
+        Shadowplay = new List<SkillInfo>();
+        Witchcraft = new List<SkillInfo>();
+    }
+
+    // csv파일로 저장된 스킬정보들을 불러온다.
+    void LoadSkillInfo()
+    {
+
     }
 }
